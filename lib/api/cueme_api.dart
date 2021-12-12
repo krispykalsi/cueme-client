831a2c5db43f519865cea63c604a4e9371e97cd7
Ikroop Singh Kalsi <ikroop.singh.kalsi@gmail.com>
2021-12-12 14:46:30 +0530

    prepare api for web deployment

--- lib/api/cueme_api.dart
+++ lib/api/cueme_api.dart
@@ -10,7 +10,13 @@ class CuemeApi {
     for (final medium in req.mediums) {
       final http.Response res = await http.post(
         Uri.parse(baseUrl + medium.name),
-        headers: {'Content-Type': 'application/json'},
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+          "Access-Control-Allow-Credentials": "true", // Required for cookies, authorization headers with HTTPS
+          "Access-Control-Allow-Headers": "Origin,Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,locale",
+          "Access-Control-Allow-Methods": "POST, OPTIONS"
+        },
         body: req.toJson(),
       );
       yield Tuple2(medium, res);
