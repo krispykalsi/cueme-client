4594979e538a4b3dd4f1a5ebfa92233de3f646f4
Ikroop Singh Kalsi <ikroop.singh.kalsi@gmail.com>
2021-12-12 14:16:10 +0530

    reconfigure alan boi

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:cueme/pages/home_page.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env.secret");
   runApp(
     const MaterialApp(
       home: HomePage(),
--- lib/pages/src/home/alan_boi.dart
+++ lib/pages/src/home/alan_boi.dart
@@ -2,6 +2,7 @@ import 'package:alan_voice/alan_voice.dart';
 import 'package:cueme/models/cueme_request.dart';
 import 'package:cueme/models/mediums.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class AlanBoi extends StatefulWidget {
   final Widget child;
@@ -15,20 +16,18 @@ class AlanBoi extends StatefulWidget {
 }
 
 class _AlanBoiState extends State<AlanBoi> {
-  dynamic email;
-  dynamic number;
-  dynamic msg;
-  dynamic date;
-  dynamic time;
-  dynamic sendEmail = false;
-  dynamic sendMsg = false;
-  dynamic sendWhatsapp = false;
+  String? email;
+  String? phone;
+  String msg = "<NO MESSAGE>";
+  String? date;
+  int? time;
+  bool sendEmail = false;
+  bool sendMsg = false;
+  bool sendWhatsapp = false;
 
   _AlanBoiState() {
-    const authKey = bool.hasEnvironment("ALAN_SDK_AUTH_KEY")
-        ? String.fromEnvironment("ALAN_SDK_AUTH_KEY")
-        : null;
-    if (authKey != null) {
+    final authKey = dotenv.get("ALAN_SDK_AUTH_KEY");
+    if (authKey.isNotEmpty) {
       AlanVoice.addButton(authKey);
       AlanVoice.callbacks.add((command) => _handleCommands(command.data));
     }
@@ -40,38 +39,48 @@ class _AlanBoiState extends State<AlanBoi> {
   }
 
   void _handleCommands(Map<String, dynamic> response) {
-    if (response["command"] == "email") {
-      email = response["data"];
+    if (response[command] == emailCmd) {
+      email = response[data];
     }
-    if (response["command"] == "phone") {
-      number = response["data"];
+    if (response[command] == phoneCmd) {
+      phone = response[data];
+      debugPrint(phone);
     }
-    if (response["command"] == "msg") {
-      msg = response["data"];
+    if (response[command] == msgCmd) {
+      msg = response[data];
     }
-    if (response["command"] == "emailfinal") {
+    if (response[command] == emailFlagCmd) {
       sendEmail = true;
     }
-    if (response["command"] == "wtspyes") {
+    if (response[command] == whatsappFlagCmd) {
       sendWhatsapp = true;
     }
-    if (response["command"] == "smsyes") {
+    if (response[command] == messageFlagCmd) {
       sendMsg = true;
     }
-    if (response["command"] == "date") {
-      date = response["data"];
+    if (response[command] == dateCmd) {
+      date = response[data];
     }
-    if (response["command"] == "time") {
-      time = response["data"];
+    if (response[command] == timeCmd) {
+      time = response[data];
     }
-    if (response["command"] == "finalCue") {
+    if (response[command] == cueCmd) {
       _onFinalCue();
     }
   }
 
   void _onFinalCue() {
-    var date = DateTime.now();
-    var time = TimeOfDay.now();
+    var dateTime = date == null ? DateTime.now() : DateTime.parse(date!);
+    TimeOfDay timeOfDay;
+    if (time == null) {
+      int secondsSinceMidnight = time ?? 0;
+      int hour = (secondsSinceMidnight / 3600) as int;
+      int minutes = ((secondsSinceMidnight / 60) as int) % 60;
+      timeOfDay = TimeOfDay(hour: hour, minute: minutes);
+    } else {
+      timeOfDay = TimeOfDay.now();
+    }
+
     var mediumSelectionState = <Mediums, bool>{};
     mediumSelectionState[Mediums.wa] = sendWhatsapp;
     mediumSelectionState[Mediums.email] = sendEmail;
@@ -82,8 +91,23 @@ class _AlanBoiState extends State<AlanBoi> {
         .map<Mediums>((isSelected) => isSelected.key)
         .toSet();
 
-    final req = CuemeRequest(msg, date, time,
-        mediums: mediums, phone: "+91" + number, email: email);
+    final req = CuemeRequest(msg, dateTime, timeOfDay,
+        mediums: mediums, phone: phone, email: email);
     widget.onCue(req);
   }
+
+  static const command = "command";
+  static const data = "data";
+
+  static const msgCmd = "msg";
+  static const phoneCmd = "phone";
+  static const emailCmd = "email";
+  static const dateCmd = "date";
+  static const timeCmd = "time";
+
+  static const whatsappFlagCmd = "whatsappFlag";
+  static const emailFlagCmd = "emailFlag";
+  static const messageFlagCmd = "messageFlag";
+
+  static const cueCmd = "cue";
 }
